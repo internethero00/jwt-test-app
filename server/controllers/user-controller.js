314927924eed00456287fc5c6da4520cf78cb4eb
internethero00 <egormix1703@gmail.com>
2025-10-05 00:00:59 +0300

    first commit: create server, client sides. mongodb connection + model, Schema + controller, route, service

---
+++ server/controllers/user-controller.js
@@ -0,0 +1,59 @@
+class userController {
+    async registration(req, res, next) {
+        try {
+
+        }
+        catch (error) {
+
+        }
+    }
+
+    async login(req, res, next) {
+        try {
+
+        }
+        catch (error) {
+
+        }
+    }
+
+    async logout(req, res, next) {
+        try {
+
+        }
+        catch (error) {
+
+        }
+    }
+
+    async activate(req, res, next) {
+        try {
+
+        }
+        catch (error) {
+
+        }
+    }
+
+    async refresh(req, res, next) {
+        try {
+
+        }
+        catch (error) {
+
+        }
+    }
+
+    async getUsers(req, res, next) {
+        try {
+            res.json('hello world')
+        }
+        catch (error) {
+
+        }
+    }
+
+
+}
+
+module.exports = new userController();
